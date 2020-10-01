1de642fe3a2cb425a4c8c8b55f7431f13fc7ad62
BIGRIG4 <Nicholas.becker@students.dominican.edu>
2020-10-01 15:14:31 -0700

    adding start script

---
+++ app.js
@@ -0,0 +1,38 @@
+// Initialize express
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
+
+const models = require('./db/models');
+require('./controllers/events')(app, models);
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
---
+++ models/controllers/event.js
@@ -0,0 +1,73 @@
+module.exports = function(app, models) {
+const moment= require('moment');
+
+    // Index
+    app.get('/', (req, res) => {
+      models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+        res.render('events-index', { events: events });
+      })
+    })
+
+    // CREATE-GET
+    app.get('/events/new', (req, res) => {
+        res.render('events-new', {});
+    })
+
+    // CREATE-POST
+    app.post('/events', (req, res) => {
+      models.Event.create(req.body).then(event => {
+          // Redirect to events/:id
+          res.redirect(`/events/${event.id}`)
+      }).catch((err) => {
+        console.log(err)
+      });
+    })
+
+    // SHOW
+    app.get('/events/:id', (req, res) => {
+      // Search for the event by its id that was passed in via req.params
+      models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then((event) => {
+        // If the id is for a valid event, show it
+        let createdAt = event.createdAt;
+        createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+        event.createdAtFormatted = createdAt;
+        res.render('events-show', { event: event })
+      }).catch((err) => {
+        // if they id was for an event not in our db, log an error
+        console.log(err.message);
+      })
+    })
+
+    // EDIT
+    app.get('/events/:id/edit', (req, res) => {
+      models.Event.findByPk(req.params.id).then((event) => {
+        res.render('events-edit', { event: event });
+      }).catch((err) => {
+        console.log(err.message);
+      })
+    });
+
+    // UPDATE
+    app.put('/events/:id', (req, res) => {
+      models.Event.findByPk(req.params.id).then(event => {
+        event.update(req.body).then(event => {
+          res.redirect(`/events/${req.params.id}`);
+        }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+
+    // DELETE
+    app.delete('/events/:id', (req, res) => {
+      models.Event.findByPk(req.params.id).then(event => {
+        event.destroy();
+        res.redirect(`/`);
+      }).catch((err) => {
+        console.log(err);
+      });
+    })
+
+}
